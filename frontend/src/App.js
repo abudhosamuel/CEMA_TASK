2e2975ea230ff9639db6be99287803c381521ffe
abudhosamuel <abudhosamuel@gmail.com>
2025-04-24 19:10:06 +0300

    made changes to navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,57 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RegisterClient from './components/RegisterClient';
 import CreateProgram from './components/CreateProgram';
 import EnrollClient from './components/EnrollClient';
 import ViewClientProfile from './components/ViewClientProfile';
 import SearchClient from './components/SearchClient';
+import { NavLink } from 'react-router-dom';
+
 
 function App() {
   return (
     <Router>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+     <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
   <div className="container">
-    <a className="navbar-brand" href="/">Health Information System</a>
+    <a className="navbar-brand text-white" href="/">Health Information System</a>
     <div>
-      <Link className="btn btn-outline-light mx-2" to="/register">Register Client</Link>
-      <Link className="btn btn-outline-light mx-2" to="/program">Create Program</Link>
-      <Link className="btn btn-outline-light mx-2" to="/enroll">Enroll Client</Link>
-      <Link className="btn btn-outline-light mx-2" to="/view">View Profile</Link>
-      <Link className="btn btn-outline-light mx-2" to="/search">Search Client</Link>
+      <NavLink 
+        to="/register" 
+        className={({ isActive }) => isActive ? "btn btn-light mx-2" : "btn btn-outline-light mx-2"}
+      >
+        Register Client
+      </NavLink>
+
+      <NavLink 
+        to="/program" 
+        className={({ isActive }) => isActive ? "btn btn-light mx-2" : "btn btn-outline-light mx-2"}
+      >
+        Create Program
+      </NavLink>
+
+      <NavLink 
+        to="/enroll" 
+        className={({ isActive }) => isActive ? "btn btn-light mx-2" : "btn btn-outline-light mx-2"}
+      >
+        Enroll Client
+      </NavLink>
+
+      <NavLink 
+        to="/view" 
+        className={({ isActive }) => isActive ? "btn btn-light mx-2" : "btn btn-outline-light mx-2"}
+      >
+        View Profile
+      </NavLink>
+
+      <NavLink 
+        to="/search" 
+        className={({ isActive }) => isActive ? "btn btn-light mx-2" : "btn btn-outline-light mx-2"}
+      >
+        Search Client
+      </NavLink>
     </div>
   </div>
 </nav>
+
 
       <Routes>
         <Route path="/register" element={<RegisterClient />} />
