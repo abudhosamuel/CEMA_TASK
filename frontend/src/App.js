b4104281509ea5e52c5ba9d15a44e69a8d7f8c86
abudhosamuel <abudhosamuel@gmail.com>
2025-04-25 13:23:36 +0300

    Added Home.js component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,59 +1,60 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
 import RegisterClient from './components/RegisterClient';
 import CreateProgram from './components/CreateProgram';
 import EnrollClient from './components/EnrollClient';
 import ViewClientProfile from './components/ViewClientProfile';
 import SearchClient from './components/SearchClient';
-import { NavLink } from 'react-router-dom';
-
+import Home from './components/Home';
 
 function App() {
   return (
     <Router>
-     <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
-  <div className="container">
-    <a className="navbar-brand text-white" href="/">Health Information System</a>
-    <div>
-      <NavLink 
-        to="/register" 
-        className={({ isActive }) => isActive ? "btn btn-light mx-2" : "btn btn-outline-light mx-2"}
-      >
-        Register Client
-      </NavLink>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+        <div className="container">
+          <NavLink to="/" className="navbar-brand text-white">
+            Health Information System
+          </NavLink>
+          <div>
+            <NavLink 
+              to="/register" 
+              className={({ isActive }) => isActive ? "btn btn-light mx-2" : "btn btn-outline-light mx-2"}
+            >
+              Register Client
+            </NavLink>
 
-      <NavLink 
-        to="/program" 
-        className={({ isActive }) => isActive ? "btn btn-light mx-2" : "btn btn-outline-light mx-2"}
-      >
-        Create Program
-      </NavLink>
+            <NavLink 
+              to="/program" 
+              className={({ isActive }) => isActive ? "btn btn-light mx-2" : "btn btn-outline-light mx-2"}
+            >
+              Create Program
+            </NavLink>
 
-      <NavLink 
-        to="/enroll" 
-        className={({ isActive }) => isActive ? "btn btn-light mx-2" : "btn btn-outline-light mx-2"}
-      >
-        Enroll Client
-      </NavLink>
+            <NavLink 
+              to="/enroll" 
+              className={({ isActive }) => isActive ? "btn btn-light mx-2" : "btn btn-outline-light mx-2"}
+            >
+              Enroll Client
+            </NavLink>
 
-      <NavLink 
-        to="/view" 
-        className={({ isActive }) => isActive ? "btn btn-light mx-2" : "btn btn-outline-light mx-2"}
-      >
-        View Profile
-      </NavLink>
+            <NavLink 
+              to="/view" 
+              className={({ isActive }) => isActive ? "btn btn-light mx-2" : "btn btn-outline-light mx-2"}
+            >
+              View Profile
+            </NavLink>
 
-      <NavLink 
-        to="/search" 
-        className={({ isActive }) => isActive ? "btn btn-light mx-2" : "btn btn-outline-light mx-2"}
-      >
-        Search Client
-      </NavLink>
-    </div>
-  </div>
-</nav>
-
+            <NavLink 
+              to="/search" 
+              className={({ isActive }) => isActive ? "btn btn-light mx-2" : "btn btn-outline-light mx-2"}
+            >
+              Search Client
+            </NavLink>
+          </div>
+        </div>
+      </nav>
 
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/register" element={<RegisterClient />} />
         <Route path="/program" element={<CreateProgram />} />
         <Route path="/enroll" element={<EnrollClient />} />
---
+++ frontend/src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="container mt-5 text-center">
+      <h1 className="display-4 mb-4"> Health Information System</h1>
+      <p className="lead">
+        Welcome to the Health Information System. <br />
+        Manage clients, health programs, enrollments, and access client profiles with ease.
+      </p>
+      <hr />
+      <p>Select an option from the navigation bar to get started.</p>
+    </div>
+  );
+}
+
+export default Home;
