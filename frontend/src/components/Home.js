81c28330e3c59214c7748be5387200c2bc382cc4
abudhosamuel <abudhosamuel@gmail.com>
2025-04-28 09:58:13 +0300

    New build

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import bgImage from './bgimage.jpeg';
+import '../App.css';
 
 function Home() {
   return (
-    <div className="container mt-5 text-center">
-      <h1 className="display-4 mb-4"> Health Information System</h1>
-      <p className="lead">
-        Welcome to the Health Information System. <br />
-        Manage clients, health programs, enrollments, and access client profiles with ease.
-      </p>
-      <hr />
-      <p>Select an option from the navigation bar to get started.</p>
+    <div 
+      className="background-container"
+      style={{ backgroundImage: `url(${bgImage})` }}
+    >
+      <div className="overlay">
+        <div className="content-box">
+          <h1 className="display-4 mb-4">Health Information System</h1>
+          <p className="lead">
+            Welcome to the Health Information System. <br />
+            Manage clients, health programs, enrollments, and access client profiles with ease.
+          </p>
+          <hr />
+          <p>Select an option from the navigation bar to get started.</p>
+        </div>
+      </div>
     </div>
   );
 }
